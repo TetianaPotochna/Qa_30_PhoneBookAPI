985977f141137adc52a265a5d756c2f5f7bfdc48
TetianaPotochna <t.potochna@gmail.com>
2025-09-21 16:44:33 +0300

    update Registration(practice)

--- src/test/java/okhttp/RegistrationTestsOkhttp.java
+++ src/test/java/okhttp/RegistrationTestsOkhttp.java
@@ -1,4 +1,8 @@
+//"application/json;charset=utf-8"
+ //"https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword"
+
 package okhttp;
+
 
 import com.google.gson.Gson;
 import dto.AuthRequestDTO;
@@ -8,31 +12,31 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.Random;
 
 public class RegistrationTestsOkhttp {
-    Gson gson = new Gson();
+Gson gson = new Gson();
+public static final MediaType JSON = MediaType.get("application/json;charset=utf-8");
+OkHttpClient client = new OkHttpClient();
 
-    public static final MediaType JSON = MediaType.get("application/json;charset=utf-8");
-    OkHttpClient okHttpClient = new OkHttpClient();
-
-    @Test
+@Test
     public void registrationSuccess() throws IOException {
-        int i = new Random().nextInt(1000)+1000;
-        AuthRequestDTO auth = AuthRequestDTO.builder()
-                .username("dusm" + i +"@gmail.com").password("Dudu12345@").build();
-        RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+    int i = (int) (System.currentTimeMillis()/1000)%3600;
+    AuthRequestDTO auth = AuthRequestDTO.builder()
+            .username("dusm" + i + "@gmail.com")
+            .password("Dudu12345@")
+            .build();
+    RequestBody body = RequestBody.create(gson.toJson(auth), JSON);
+    Request request = new Request.Builder()
+            .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
+            .post(body)
+            .build();
 
-        Request request = new Request.Builder()
-                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
-                .post(body)
-                .build();
-Response response = okHttpClient.newCall(request).execute();
+    Response response = client.newCall(request).execute();
+    Assert.assertTrue(response.isSuccessful());
+    Assert.assertEquals(response.code(), 200);
 
-Assert.assertTrue(response.isSuccessful());
-Assert.assertEquals(response.code(), 200);
+    AuthResponseDTO responseDTO = gson.fromJson(response.body().string(), AuthResponseDTO.class);
+    System.out.println(responseDTO.getToken());
+}
 
-AuthResponseDTO responseDTO = gson.fromJson(response.body().string(), AuthResponseDTO.class);
-        System.out.println(responseDTO.getToken());
-    }
 }
